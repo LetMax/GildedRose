b892b7b19136a6eb8839fa983a193f5cc62476a5
LetMax <leterremaxime@gmail.com>
2019-11-05 16:58:05 +0100

    simplification de la gestion du sellIn

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,15 +9,16 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 continue;
             }
 
             if (item.name.equals("Aged Brie")) {
                 augmenterQualite(item);
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            } 
+            else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
                 augmenterQualite(item);
+
                 if (item.sellIn < 11) {
                     augmenterQualite(item);
                 }
@@ -25,21 +26,22 @@ class GildedRose {
                 if (item.sellIn < 6) {
                     augmenterQualite(item);
                 }
-            }else {
+            }
+            else {
                 diminuerQualite(item);
             }
 
             item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        diminuerQualite(item);
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.name.equals("Aged Brie")) {
                     augmenterQualite(item);
+                }
+                else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
+                } 
+                else {
+                    diminuerQualite(item);
                 }
             }
         }
